3daf4f49752c7617fdc5917c2a653c2c1e779c97
Jewei Mak <jewei.mak@gmail.com>
2016-02-06 11:32:58 +0800

    Tutorial 2 & 3

--- src/index.js
+++ src/index.js
@@ -4,10 +4,32 @@ import ReactDOM from 'react-dom'
 class CommentBox extends React.Component {
   render() {
     return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
+      <div className="comment-box">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
-    );
+    )
+  }
+}
+
+class CommentList extends React.Component {
+  render () {
+    return (
+      <div className="comment-list">
+        Hello, world! I am a Commentlist.
+      </div>
+    )
+  }
+}
+
+class CommentForm extends React.Component {
+  render () {
+    return (
+      <div className="comment-form">
+        Hello, world! I am a Commentform.
+      </div>
+    )
   }
 }
 
