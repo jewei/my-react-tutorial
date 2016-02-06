724e2019d6a1b06a67e053e583a0880355f21525
Jewei Mak <jewei.mak@gmail.com>
2016-02-06 12:09:46 +0800

    Tutorial 5 $ 6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+const marked = require('marked')
 
 class CommentBox extends React.Component {
   render() {
@@ -17,7 +18,8 @@ class CommentList extends React.Component {
   render () {
     return (
       <div className="comment-box__list">
-        Hello, world! I am a Commentlist.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     )
   }
@@ -28,6 +30,19 @@ class CommentForm extends React.Component {
     return (
       <div className="comment-box__form">
         Hello, world! I am a Commentform.
+      </div>
+    )
+  }
+}
+
+class Comment extends React.Component {
+  render () {
+    return (
+      <div className="comment-box__comment">
+        <h2 className="comment-box__author">
+          {this.props.author}
+        </h2>
+        {marked(this.props.children.toString())}
       </div>
     )
   }
